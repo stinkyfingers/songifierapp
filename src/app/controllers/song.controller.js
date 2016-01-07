a2c1bfa63299c8a7e1bbbde88dfafa6f194de852
stinkyfingers <john_shenk@hotmail.com>
2016-01-07 07:10:21 -0600

    song editing

--- src/app/controllers/song.controller.js
+++ src/app/controllers/song.controller.js
@@ -1,9 +1,46 @@
 export class SongController {
-  constructor () {
+  constructor (SongService) {
     'ngInject';
-
-    
+    this.$song = SongService;
+    this.genres = this.getGenres();
+    this.save = () => {this.save(this.song)};
+    this.remove = () =>{ this.remove(this.song)};
   }
+
+	getGenres (){
+		this.$song.getGenres().then((resp) => {
+			this.genres = resp.data;
+		}, (err) => {
+
+		});
+	}
+
+	save(song){
+		if (!song){
+			//err
+		}
+		if (song.id){
+			this.$song.create(song).then((resp) => {
+				this.song = resp.data;
+			}, (err) => {
+
+			});
+		} else{
+			this.$song.update(song).then((resp) => {
+				this.song = resp.data;
+			}, (err) => {
+
+			});
+		}
+	}
+
+	remove(song){
+		this.$song.remove(song).then(()=> {
+
+		}, (err) => {
+
+		});
+	}
 
   
 }
--- src/app/services/song.service.js
+++ src/app/services/song.service.js
@@ -17,4 +17,49 @@ export class SongService{
 			data:{}
 		})
 	}
+
+	create(song){
+		return this.$http({
+			method: 'POST',
+			url: this.api + '/song',
+			headers: {
+				'Token': 'Bearer: ' + this.$app.getToken()
+			},
+			data:song
+		})
+	}
+
+	update(song){
+		return this.$http({
+			method: 'PUT',
+			url: this.api + '/song',
+			headers: {
+				'Token': 'Bearer: ' + this.$app.getToken()
+			},
+			data:song
+		})
+	}
+
+
+	remove(song){
+		return this.$http({
+			method: 'DELETE',
+			url: this.api + '/song',
+			headers: {
+				'Token': 'Bearer: ' + this.$app.getToken()
+			},
+			data:song
+		})
+	}
+
+
+	getGenres(){
+		return this.$http({
+			method: 'GET',
+			url: this.api + '/genres',
+			headers: {
+				'Token': 'Bearer: ' + this.$app.getToken()
+			}
+		})
+	}
 }
