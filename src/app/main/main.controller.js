ac514d2e2e6c9b975bbce7914234c739507dec11
stinkyfingers <john_shenk@hotmail.com>
2016-01-09 09:32:43 -0600

    adds voting

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -5,17 +5,60 @@ export class MainController {
     this.$song = SongService;
     this.$user = UserService;
     this.$app = AppService;
-    this.log_in = () => {this.login(this.creds)};
-    this.log_out = () => {this.logout()};
+    this.log_in = () => {this.login(this.creds);};
+    this.log_out = () => {this.logout();};
     this.user = this.getUser();
     this.songs = this.getSongs();
+    this.vote_options = [1,2,3,4,5];
+    this.vote = (song, option) => {this.voteOnSong(song, option);};
   }
 
   getSongs(){
     this.$song.getSongs().then((resp) => {
       this.songs = resp.data;
+      for (let i in this.songs){
+        this.songs[i].total = 0;
+        let count = 0;
+        for (let j in this.songs[i].votes){
+          this.songs[i].total += this.songs[i].votes[j].rank;
+          if (this.songs[i].votes[j].rank && this.songs[i].votes[j].rank > 0){
+            count++;
+          }
+          if (this.songs[i].votes[j].user && this.songs[i].votes[j].user.id === this.user.id){
+            this.songs[i].user_vote = this.songs[i].votes[j].rank;
+          }
+        }
+        if (count){
+          this.songs[i].total = this.songs[i].total/(count);
+        }
+      }
     }, (err) => {
       this.$app.error(err.data);
+    });
+  }
+
+  voteOnSong(song, option){
+    if (!option){
+      this.$app.error('Invalid value.');
+      return;
+    }
+    for (let i in song.votes){
+      if (song.votes[i].user.id === this.user.id){
+        song.votes[i].rank = parseInt(option);
+        return this.updateSong(song);
+      }
+    }
+    if (!song.votes){
+      song.votes = [];
+    }
+    song.votes.push({rank: parseInt(option), user: this.user});
+        return this.updateSong(song);
+  }
+
+  updateSong(song){
+    this.$song.update(song).then(() => {
+    }, (err) => {
+      this.$app.error(err);
     });
   }
 
